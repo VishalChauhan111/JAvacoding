a070297fbf63f724df78302d75c080a26bbe2170
VishalChauhan111 <147490964+VishalChauhan111@users.noreply.github.com>
2026-01-17 01:06:20 +0530

    Add files via upload

--- QueueY.java
+++ QueueY.java
@@ -1,6 +1,43 @@
 import java.util.ArrayDeque;
+import java.util.Stack;
 
 public class QueueY {
+
+    static Stack<Integer> s1 = new Stack<>();
+    static Stack<Integer> s2 = new Stack<>();
+
+    public static boolean isEmpty(){
+        return s1.isEmpty();
+    }
+
+    public static void add(int data){
+        while (!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+
+        s1.push(data);
+
+        while (!s2.isEmpty()){
+            s2.push(s1.pop());
+        }
+    }
+
+    public static int remove(){
+        if (isEmpty()){
+            System.out.println("empty queue");
+            return  -1;
+        }
+         return s1.pop();
+    }
+
+    public static int peek(){
+        if (isEmpty()){
+            System.out.println("empty queue");
+            return  -1;
+        }
+        return s1.peek();
+    }
+
 
     static class Node {
         int data;
