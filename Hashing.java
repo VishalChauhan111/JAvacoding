062d955d513e09d939b27735ed1d09d1281a038d
VishalChauhan111 <147490964+VishalChauhan111@users.noreply.github.com>
2026-02-01 20:21:38 +0530

    Add files via upload

---
+++ Hashing.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Hashing {
+    public static void main(String[] args) {
+        //Creation
+        HashSet<Integer> set = new HashSet<>();
+
+        //Insert
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        //Size
+        System.out.println(set.size());
+
+        //Print all element
+        System.out.println(set);
+
+        //Iterator
+        Iterator it = set.iterator();
+        //hasNext; next
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        //Search - contain
+        if (set.contains(1)){
+            System.out.println("set contain 1");
+        }
+
+        if (!set.contains(6)){
+            System.out.println("does not contains");
+        }
+
+        //Delete
+        set.remove(1);
+        if (!set.contains(1)){
+            System.out.println("does not contain 1 - we deleted 1");
+        }
+    }
+}
